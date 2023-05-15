d769399d5dfe7a4442db410d5013614ad0887f2c
Jhon Fabio Cardona Martinez <jfcmdev@gmail.com>
2023-05-15 17:00:34 -0500

    Implemnetacion de la ruta para obtener articulos indivuales

---
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_can_fetch_a_single_article()
+    {
+        $this->withoutExceptionHandling();
+
+        $article = Article::factory()->create();
+
+        $response =  $this->getJson(route('api.v1.articles.show', $article->getRouteKey()));
+
+        // $response->assertSee($article->title);
+        $response->assertJson([
+            'data' => [
+                'type' => 'articles',
+                'id' => (string) $article->getRouteKey(),
+                'attributes' => [
+                    'title' => $article->title,
+                    'slug' => $article->slug,
+                    'content' => $article->content,
+                ],
+                'links' => [
+                    'self' => url(route('api.v1.articles.show', $article->getRouteKey()))
+                ]
+            ]
+        ]);
+    }
+}
