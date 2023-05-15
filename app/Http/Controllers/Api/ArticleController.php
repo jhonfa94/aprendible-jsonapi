a3e21b52e9906e58a8e5cc005a6b5709ad5de32d
Jhon Fabio Cardona Martinez <jfcmdev@gmail.com>
2023-05-15 17:21:18 -0500

    implementando la ruta para crear articulos

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+
+
+
     public function show(Article $article)
     {
         // return $article;
@@ -27,5 +31,11 @@ class ArticleController extends Controller
         //     ]
         // ]);
         return ArticleResource::make($article);
+    }
+
+    public function index()
+    {
+        $articles = Article::all();
+        return ArticleCollection::make($articles);
     }
 }
